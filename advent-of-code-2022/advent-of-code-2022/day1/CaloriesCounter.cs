108c41f983c186aa0d32967ebce2e28f85e73afd
Bruno Silva <brunosilva@Brunos-MBP.lan>
2022-12-01 18:54:19 -0300

    day 1

---
+++ advent-of-code-2022/advent-of-code-2022/day1/CaloriesCounter.cs
@@ -0,0 +1,50 @@
+﻿namespace advent_of_code_2022.day1
+{
+	public class CaloriesCounter
+	{
+
+		public int FindElfWithGreatestCalloriesReserve()
+		{
+			int accumulator = 0, result = 0;
+
+            foreach (var line in File.ReadAllLines("./day1/input.txt"))
+			{
+				if (line.Length == 0)
+				{
+					result = result < accumulator ? accumulator : result;
+					accumulator = 0;
+					continue;
+				}
+
+				accumulator += int.Parse(line);
+			}
+
+			return result;
+		}
+
+
+        public int FindTopThreeElvesByCalloriesReserve()
+        {
+            var accumulator = 0;
+            var caloriesPerElf = new List<int>();
+
+            foreach (var line in File.ReadAllLines("./day1/input.txt"))
+            {
+                if (line.Length == 0)
+                {
+                    caloriesPerElf.Add(accumulator);
+                    accumulator = 0;
+                    continue;
+                }
+
+                accumulator += int.Parse(line);
+            }
+
+            var top3 = caloriesPerElf.OrderByDescending(c => c).ToArray();
+
+            return top3.Take(3).Sum();
+        }
+
+    }
+}
+
