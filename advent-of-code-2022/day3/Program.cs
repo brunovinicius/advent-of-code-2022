98f49bf294dc9225ed4eb61afc91b02b2814f433
Bruno Silva <brunosilva@Brunos-MBP.lan>
2022-12-04 08:38:15 -0300

    day 3 and 4

---
+++ advent-of-code-2022/day3/Program.cs
@@ -0,0 +1,52 @@
+﻿global using System;
+global using System.Linq;
+
+Console.WriteLine("Advent of Code 2022");
+Console.WriteLine("------ Day 3 ------");
+
+var result1 = FindMissplacedRucksackItems();
+Console.WriteLine($"Day 3-1: {result1}");
+
+var result2 = FindElfGroupBadgePriorities();
+Console.WriteLine($"Day 3-2: {result2}");
+
+int FindMissplacedRucksackItems()
+{
+    var lines = File.ReadAllLines("./input.txt");
+    
+    return lines
+        .Select(s => s.Chunk(s.Length / 2))
+        .Select(chunks => chunks.First().Intersect(chunks.Last()).SingleOrDefault())
+        .Where(c => c != 0)
+        .Select(c =>
+            c >= 'a'
+                ? c - 'a' + 1
+                : c - 'A' + 27
+        )
+        .Sum();
+}
+
+
+int FindElfGroupBadgePriorities()
+{
+    var lines = File.ReadAllLines("./input.txt");
+
+    return lines
+        .Chunk(3)        
+        .Select(chunks =>
+            chunks
+                .Skip(1)
+                .Aggregate(
+                    new HashSet<char>(chunks.First().ToCharArray()),
+                    (h, e) => { h.IntersectWith(e); return h; }
+                )
+                .SingleOrDefault()
+        )
+        .Where(c => c != 0)
+        .Select(c =>
+            c >= 'a'
+                ? c - 'a' + 1
+                : c - 'A' + 27
+        )
+        .Sum();
+}
---
+++ advent-of-code-2022/day4/Program.cs
@@ -0,0 +1,66 @@
+﻿Console.WriteLine("Advent of Code 2022");
+Console.WriteLine("------ Day 4 ------");
+
+var result1 = FindFullyOverlapingAssignmentPairs();
+Console.WriteLine($"Day 4-1: {result1}");
+
+var result2 = FindOverlapingAssignmentPairs();
+Console.WriteLine($"Day 4-2: {result2}");
+
+
+int FindFullyOverlapingAssignmentPairs()
+{
+    var lines = File.ReadAllLines("./input.txt");
+
+    return lines
+        .Select(l => l.Split(','))
+        .Select(pair => pair
+            .Select(s => s.Split('-'))
+            .Select(n => new Range(int.Parse(n[0]), int.Parse(n[1])))
+            .ToArray()
+        )
+        .Select(ranges => new { first = ranges[0], second = ranges[1] })
+        .Select(assignmentPair =>
+            assignmentPair.first.Contains(assignmentPair.second)
+                || assignmentPair.second.Contains(assignmentPair.first)
+        )
+        .Where(result => result)
+        .Count();
+}
+
+int FindOverlapingAssignmentPairs()
+{
+    var lines = File.ReadAllLines("./input.txt");
+
+    return lines
+        .Select(l => l.Split(','))
+        .Select(pair => pair
+            .Select(s => s.Split('-'))
+            .Select(n => new Range(int.Parse(n[0]), int.Parse(n[1])))
+            .ToArray()
+        )
+        .Select(ranges => new { first = ranges[0], second = ranges[1] })
+        .Select(assignmentPair =>
+            assignmentPair.first.Overlap(assignmentPair.second)
+                || assignmentPair.second.Overlap(assignmentPair.first)
+        )
+        .Where(result => result)
+        .Count();
+}
+
+class Range
+{
+    public readonly int Low, High;
+
+    public Range(int low, int high)
+    {
+        Low = low;
+        High = high;
+    }
+
+    public bool Contains(Range range) => Contains(range.Low) && Contains(range.High);
+
+    public bool Contains(int number) => number >= Low && number <= High;
+
+    public bool Overlap(Range range) => Contains(range.Low) || Contains(range.High);
+}
