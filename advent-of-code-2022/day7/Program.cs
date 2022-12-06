8621eee599b5614d11313e29859ce55f6014812f
Shift <bruno.vinicius.ds@gmail.com>
2022-12-06 05:29:14 -0300

    Adding project for next couple days :joy:

---
+++ advent-of-code-2022/day7/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Advent of Code 2022");
+Console.WriteLine("------ Day 7 ------");
+
+var result1 = puzzle1();
+Console.WriteLine($"Day 7-1: {result1}");
+
+var result2 = puzzle2();
+Console.WriteLine($"Day 7-2: {result2}");
+
+int puzzle1()
+{
+    return 0;
+}
+
+int puzzle2()
+{
+    return 0;
+}
